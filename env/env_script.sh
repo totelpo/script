1a7a3a47987e47e16fed970851bfe3977922d9d7
totelpo <tots.po@gmail.com>
2024-09-06 18:14:08 +0800

    Update env_script.sh

--- env/env_script.sh
+++ env/env_script.sh
@@ -1,3 +1,5 @@
+# This env file shall be called by every script to easily define temp files(output, logs, etc.)
+
 p_all_input_parameters="$@"
 
 # sc_name=$0 # must be declared from the parent script to gets its full path
@@ -7,5 +9,6 @@ sc_name2="${sc_name1%.*}"       # scriptname without extension name
 sc_dirname=$(dirname $sc_name)
 sc_tmp=${TMPDIR}/$sc_name2
 
+# This will help identify which script is currently executing.
 f-marker $sc_name1 $p_all_input_parameters
 
