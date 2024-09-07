c8da0b1ab0630a4c442f8244816df1f4eba17d80
totelpo <tots.po@gmail.com>
2024-09-07 22:29:47 +0800

    Updated/created file/s via f-git-add-updated-last-n-minutes n=5 : function/f-git-add-updated-last-n-minutes.sh

---
+++ function/f-git-add-updated-last-n-minutes.sh
@@ -0,0 +1,39 @@
+f-git-add-updated-last-n-minutes(){
+  fn_name=${FUNCNAME[0]}
+  p_github_repo="$1";   p_github_repo=${p_github_repo:=default} # default value
+  p_minutes_ago="$2";   p_minutes_ago=${p_minutes_ago:=5} # default value
+  GITHUB_DIR=${GITHUB_DIR:=/github/totelpo}
+  v_repo_dir=${GITHUB_DIR}/${p_github_repo}
+  v_out=${TMPDIR}/${FUNCNAME[0]}-${p_github_repo}.sh
+  f_use(){
+    cat << EOF
+USAGE : 
+${fn_name} GITHUB_REPO  MINUTES_AGO
+${fn_name} script       5
+${fn_name} demo         5
+EOF
+  }
+  if [ $# -eq 2 ]; then
+    if [ ${p_minutes_ago} -gt 0 ]; then
+      if [ -d ${v_repo_dir} ]; then
+        cd ${v_repo_dir}
+        v_files_changed_today=$(echo `find -type f -mmin -${p_minutes_ago} | grep -v '\.git' | sed 's|^\./||' | grep -v '\.swp$' | sort`)
+        cat << EOF > ${v_out}
+git remote set-url origin git@github.com:totelpo/${p_github_repo}.git
+git add ${v_files_changed_today}
+git commit -m "Updated/created file/s via ${fn_name} n=${p_minutes_ago} : ${v_files_changed_today}"
+git push origin main
+EOF
+        sh -xc "cat ${v_out}"
+      else
+        echo "Repo folder does not exists."
+        ls -lhd ${v_repo_dir}
+      fi
+    else
+      echo "p_minutes_ago must be greater than 0"
+    fi
+  else
+    echo "No repo defined. Check GITHUB_DIR=${GITHUB_DIR}"
+    f_use
+  fi
+}
