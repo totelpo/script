24667882193cf2c1d29d4b2aeb45c30b07df4c62
totelpo <tots.po@gmail.com>
2024-09-09 20:30:38 +0800

    Add/Update vm-remote-env.list vm-remote-function.list vm-copy-scripts.sh

---
+++ kvm/vm-copy-scripts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# totel 20240909 Create script and add to github
+sc_name=$0
+
+source ${ENV_DIR}/env_function.sh
+source ${ENV_DIR}/env_script.sh
+
+v_log=$sc_tmp.log
+
+f_use() {
+        echo "
+ DESC : This script will copy scripts needed by the VM
+USAGE : 
+IP=192.168.122.90 $sc_name1
+" | sed "s|$HOME|~|g"
+
+exit
+}
+
+if [ ! -z "${IP}" ]; then
+  f-marker $sc_name1 $p_all_input_parameters
+else
+  f_use
+fi
+
+set -e 
+f_main(){
+  v_dir=$1
+  v_file_list=vm-remote-${v_dir}.list
+cd ${SCRIPT_DIR}/${v_dir}/
+if [ ! -f ${v_file_list} ]; then
+  ls -lh ${v_file_list}
+  exit 1
+fi
+v_list="`echo $(grep -v '^#' ${v_file_list} )`"
+sh -xc "rsync -a ${v_list} ${IP}:script/${v_dir}/"
+}
+
+f_main function
+f_main env
+
