9a27452574c760825f1222fd9920c37fb4ef9388
totelpo <tots.po@gmail.com>
2024-09-10 23:10:54 +0800

    Update f-el-kickstart-update.sh

--- function/f-el-kickstart-update.sh
+++ function/f-el-kickstart-update.sh
@@ -1,16 +1,16 @@
 f-el-kickstart-update(){
 f_tmp_ks_update=/tmp/${FUNCNAME[0]}.tmp
 f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3 $4 $5"
-echo "-z $VM -o -z $KS_TMP -o -z $NETDEV -o -z $PROTO"
-if [ -z $VM -o -z $KS_TMP -o -z $NETDEV -o -z $PROTO ] ; then 
-	echo -e "\n[ERROR] Empty variable.\n" 
+echo "-z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_DOMAIN}"
+if [ -z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_DOMAIN} ] ; then 
+	echo -e "\n[ERROR] Empty variable found.\n" 
 	cat << EOF
 # USAGE : 
 # This script is called from kvm-el.sh
 VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=dhcp ${FUNCNAME[0]} 
 VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=static  IP=192.168.122.70 ${FUNCNAME[0]} 
 EOF
-	exit
+	exit 1
 else
 	if [ ! -f ${KS_TMP} ]; then
 		ls -lh ${KS_TMP}
