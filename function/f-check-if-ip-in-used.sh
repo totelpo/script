14781a7832f3f5951a4d08c7b4bc229d43949cc2
totelpo <tots.po@gmail.com>
2024-09-10 16:05:19 +0800

    Add f-check-if-ip-in-used.sh

---
+++ function/f-check-if-ip-in-used.sh
@@ -0,0 +1,14 @@
+f-check-if-ip-in-used(){
+	f-message EXECUTING "${FUNCNAME[0]} $1 $2"
+	fi_ip=$1
+	fi_exit="$2"; fi_exit=${fi_exit:=y}
+	ping -c 1 $fi_ip > /dev/null
+	fx_check_if_ip_in_used=$?
+	if [ $fx_check_if_ip_in_used -eq 0 ]; then
+		echo -e "IP $fi_ip is alive."
+		if [ "$fi_exit" = "y" ]; then
+			exit 1
+		fi
+	fi
+	echo -e "Passed."
+}
