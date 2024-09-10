8a60b3512845da304d5f57cb290c902a1647df6c
totelpo <tots.po@gmail.com>
2024-09-10 23:10:54 +0800

    Update f-passwordless-ssh.sh

--- function/f-passwordless-ssh.sh
+++ function/f-passwordless-ssh.sh
@@ -1,29 +1,27 @@
 
 f-passwordless-ssh(){
 f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3"
-if [ $# -eq 3 ]; then
-        fi_template=$1
-        fi_vm_name="$2"
-        fi_os_user="$3"  ; fi_os_user=${fi_os_user:=root}
+if [ $# -eq 2 ]; then
+        fi_os=$1
+        fi_ip="$2"
         set -e
-        f-get-kvm-ip $fi_vm_name # return r_ip
-        echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R $r_ip"
+        echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${fi_ip}"
         echo
-        if [ "$fi_template" = "el5" ]; then
-                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm -o HostKeyAlgorithms=+ssh-rsa $fi_os_user@$r_ip"
+        if [ "${fi_os}" = "el5" ]; then
+                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm -o HostKeyAlgorithms=+ssh-rsa ${fi_ip}"
         else
-                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm $fi_os_user@$r_ip"
+                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm ${fi_ip}"
         fi
         echo -e "\n# Test passwordless SSH :"
-        sh -xc "ssh -i ${HOME}/.ssh/id_rsa_kvm $fi_os_user@$r_ip id"
+        sh -xc "ssh -i ${HOME}/.ssh/id_rsa_kvm ${fi_ip} id"
 else
 	cat << EOF
 # CALL first :
 #vm-arp-clear-unreachable-ip.sh
 #f-ansible-hosts-kvm
 # USAGE : 
-${FUNCNAME[0]} OS  VM_NAME OS_USER
-${FUNCNAME[0]} el7 c7-070  root
+${FUNCNAME[0]} OS  IP
+${FUNCNAME[0]} el9 192.168.122.90
 EOF
 fi
 }
