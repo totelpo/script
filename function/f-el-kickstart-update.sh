86b9886443c507d28f2d2a2301ac586a4ef5359e
totelpo <tots.po@gmail.com>
2024-09-11 13:15:24 +0800

    Update f-el-kickstart-update.sh

--- function/f-el-kickstart-update.sh
+++ function/f-el-kickstart-update.sh
@@ -1,8 +1,8 @@
 f-el-kickstart-update(){
 f_tmp_ks_update=/tmp/${FUNCNAME[0]}.tmp
 f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3 $4 $5"
-echo "-z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_DOMAIN}"
-if [ -z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_DOMAIN} ] ; then 
+echo "-z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_OS_ADMIN}"
+if [ -z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_OS_ADMIN} ] ; then 
 	echo -e "\n[ERROR] Empty variable found.\n" 
 	cat << EOF
 # USAGE : 
@@ -22,11 +22,18 @@ else
 	else
 		 v_ip_param=
 	fi
+  if [ -z ${VM_DOMAIN} ]; then
+    v_hostname=$VM
+  else
+    v_hostname=$VM
+  fi
 	cat << EOF > $f_tmp_ks_update.sh
-sed -i "s|^network .*|network --device $NETDEV --bootproto $PROTO $v_ip_param --noipv6 --netmask 255.255.255.0 --gateway 192.168.122.1 --nameserver 192.168.122.1,8.8.8.8 --hostname $VM.${VM_DOMAIN}|g" ${KS_TMP}
+sed -i "s|^network .*|network --device $NETDEV --bootproto $PROTO $v_ip_param --noipv6 --netmask 255.255.255.0 --gateway 192.168.122.1 --nameserver 192.168.122.1,8.8.8.8 --hostname ${v_hostname}|g" ${KS_TMP}
+sed -i "s|osadmin|${VM_OS_ADMIN}|g" ${KS_TMP}
 sh -xc "grep '^network' ${KS_TMP}"
 EOF
 	EXEC=y f-exec-temp-script $f_tmp_ks_update.sh
+  sh -xc 'sleep 5'
 fi
 }
 
