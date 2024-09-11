a92d7ca7793fac41b45ea7d473ff7a48fd9d318e
totelpo <tots.po@gmail.com>
2024-09-11 17:12:08 +0800

    Update passwordless-ssh.sh

--- kvm/passwordless-ssh.sh
+++ kvm/passwordless-ssh.sh
@@ -1,28 +1,28 @@
+#!/bin/bash
+sc_name=$0
+source ${ENV_DIR}/env_function.sh
+source ${ENV_DIR}/env_script.sh
 
-f-passwordless-ssh(){
-f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3"
-if [ $# -eq 2 ]; then
-        fi_os=$1
-        fi_ip="$2"
-        set -e
-        echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${fi_ip}"
-        echo
-        if [ "${fi_os}" = "el5" ]; then
-                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm -o HostKeyAlgorithms=+ssh-rsa ${fi_ip}"
-        else
-                sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm ${fi_ip}"
-        fi
-        echo -e "\n# Test passwordless SSH :"
-        sh -xc "ssh -i ${HOME}/.ssh/id_rsa_kvm ${fi_ip} id"
-else
-	cat << EOF
-# CALL first :
-#vm-arp-clear-unreachable-ip.sh
-#f-ansible-hosts-kvm
-# USAGE : 
-${FUNCNAME[0]} OS  IP
-${FUNCNAME[0]} el9 192.168.122.90
-EOF
-fi
+f_use(){
+          echo "
+ DESC: This is a template script
+USAGE:
+OS=el9 IP=192.168.122.90 $sc_name1
+"
 }
 
+if [ ! -z "${OS}" -o ! -z "${IP}" ]; then
+  f-marker $sc_name1 $p_all_input_parameters
+  set -e
+  echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${IP}"
+  echo
+  if [ "${OS}" = "el5" ]; then
+          sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm -o HostKeyAlgorithms=+ssh-rsa ${IP}"
+  else
+          sh -xc "ssh-copy-id -i ${HOME}/.ssh/id_rsa_kvm ${IP}"
+  fi
+  echo -e "\n# Test passwordless SSH :"
+  sh -xc "ssh -i ${HOME}/.ssh/id_rsa_kvm ${IP} id"
+else
+  f_use
+fi
