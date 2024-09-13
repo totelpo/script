f68e4230c6a0aa10b8f345ec905286453795395c
totelpo <tots.po@gmail.com>
2024-09-13 19:56:03 +0800

    Update f-el-kickstart-update.sh

--- function/f-el-kickstart-update.sh
+++ function/f-el-kickstart-update.sh
@@ -1,14 +1,16 @@
 f-el-kickstart-update(){
-f_tmp_ks_update=/tmp/${FUNCNAME[0]}.tmp
-f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3 $4 $5"
-echo "-z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_OS_ADMIN}"
+fn_name=${FUNCNAME[0]}
+fn_tmp=${TMPDIR}/${fn_name}-tmp
+f-marker $fn_name $@
+
 if [ -z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_OS_ADMIN} ] ; then 
-	echo -e "\n[ERROR] Empty variable found.\n" 
+	echo -e "\n[ERROR] Empty variable/s found.\n" 
+  echo "-z ${VM} -o -z ${KS_TMP} -o -z ${NETDEV} -o -z ${PROTO} -o -z ${VM_OS_ADMIN}"
 	cat << EOF
 # USAGE : 
 # This script is called from kvm-el.sh
-VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=dhcp ${FUNCNAME[0]} 
-VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=static  IP=192.168.122.70 ${FUNCNAME[0]} 
+VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=dhcp ${fn_name} 
+VM=c7-070  KS_TMP={TMPDIR}/el7.ks  NETDEV=eth0   PROTO=static  IP=192.168.122.70 ${fn_name} 
 EOF
 	exit 1
 else
@@ -27,13 +29,12 @@ else
   else
     v_hostname=$VM
   fi
-	cat << EOF > $f_tmp_ks_update.sh
+	cat << EOF > $fn_tmp.sh
 sed -i "s|^network .*|network --device $NETDEV --bootproto $PROTO $v_ip_param --noipv6 --netmask 255.255.255.0 --gateway 192.168.122.1 --nameserver 192.168.122.1,8.8.8.8 --hostname ${v_hostname}|g" ${KS_TMP}
 sed -i "s|osadmin|${VM_OS_ADMIN}|g" ${KS_TMP}
 sh -xc "grep '^network' ${KS_TMP}"
 EOF
-	EXEC=y f-exec-temp-script $f_tmp_ks_update.sh
-  sh -xc 'sleep 5'
+	EXEC=y COLUMNS=95 f-exec-temp-script $fn_tmp.sh
 fi
 }
 
