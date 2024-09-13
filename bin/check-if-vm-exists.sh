005c75a718fd5316e0d040bb8eb177598df91ace
totelpo <tots.po@gmail.com>
2024-09-13 16:33:23 +0800

    Update check-if-vm-exists.sh

--- bin/check-if-vm-exists.sh
+++ bin/check-if-vm-exists.sh
@@ -1,11 +1,28 @@
-f-check-if-vm-exists(){
-	f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3"
-	fi_vm_name=$1
-	virsh list --all | grep " $fi_vm_name "
-	fx_check_if_vm_exists=$?
-	if [ $fx_check_if_vm_exists -eq 0 ]; then
-		echo -e "\nFAILED : VM $fi_vm_name already exists.\n"
-		exit
-	fi
-	echo -e "PASSED."
+#!/bin/bash
+sc_name=$0
+source ${ENV_DIR}/env_function.sh
+source ${ENV_DIR}/env_script.sh
+
+f_use(){
+          echo "
+ DESC: Check if VM exists
+USAGE:"
+  cat << EOF | column -t
+VM=el9-090 $sc_name1
+EOF
+exit
 }
+
+if [ ! -z "${VM}" ]; then # if required variables are not empty
+  f-marker $sc_name1 $p_all_input_parameters
+else
+  f_use
+fi
+
+virsh list --all | grep " ${VM} "
+fx_check_if_vm_exists=$?
+if [ $fx_check_if_vm_exists -eq 0 ]; then
+  echo -e "\nFAILED : VM ${VM} already exists.\n"
+  exit 1
+fi
+echo -e "PASSED."
