3b531e4e85369a2dcaad8e8829ecab77d916f2e4
totelpo <tots.po@gmail.com>
2024-09-13 13:08:15 +0800

    Update f-check-if-similar-vm-exists-based-on-ip.sh

--- function/f-check-if-similar-vm-exists-based-on-ip.sh
+++ function/f-check-if-similar-vm-exists-based-on-ip.sh
@@ -3,11 +3,11 @@ f-check-if-similar-vm-exists-based-on-ip(){
 	fi_ip=$1
 	v_ip_last=`echo $fi_ip | awk -F'.' '{ print $NF }'`
 	v_ip_last_padded=`printf "%03d" $v_ip_last`
-	virsh list --all | grep "\-$v_ip_last_padded"
+	virsh list --all | grep "\-$v_ip_last_padded" 2> /dev/null
 	fx_check_if_similar_vm_exists_based_on_ip=$?
 	if [ $fx_check_if_similar_vm_exists_based_on_ip -eq 0 ]; then
 		echo -e "\nVM $fi_vm_name has similar VM/s based on IP $fi_ip\n"
 		exit
 	fi
-	echo -e "Passed.\n"
+	echo -e "PASSED."
 }
