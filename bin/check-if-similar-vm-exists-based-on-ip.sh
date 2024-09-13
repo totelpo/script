3445ce8c4472deb2eadc1cbea111bf2c82ef1de4
totelpo <tots.po@gmail.com>
2024-09-13 16:33:23 +0800

    Update check-if-similar-vm-exists-based-on-ip.sh

--- bin/check-if-similar-vm-exists-based-on-ip.sh
+++ bin/check-if-similar-vm-exists-based-on-ip.sh
@@ -1,13 +1,34 @@
-f-check-if-similar-vm-exists-based-on-ip(){
-	f-message EXECUTING "${FUNCNAME[0]} $1 $2 $3"
-	fi_ip=$1
-	v_ip_last=`echo $fi_ip | awk -F'.' '{ print $NF }'`
-	v_ip_last_padded=`printf "%03d" $v_ip_last`
-	virsh list --all | grep "\-$v_ip_last_padded" 2> /dev/null
-	fx_check_if_similar_vm_exists_based_on_ip=$?
-	if [ $fx_check_if_similar_vm_exists_based_on_ip -eq 0 ]; then
-		echo -e "\nVM $fi_vm_name has similar VM/s based on IP $fi_ip\n"
-		exit
-	fi
-	echo -e "PASSED."
+#!/bin/bash
+# totel 20240913 Convert function to script : 'mv function/f-check-if-similar-vm-exists-based-on-ip.sh bin/check-if-similar-vm-exists-based-on-ip.sh'
+
+sc_name=$0
+source ${ENV_DIR}/env_function.sh
+source ${ENV_DIR}/env_script.sh
+
+# f-marker $sc_name1 $p_all_input_parameters  # move after all the checks
+
+f_use(){
+          echo "
+ DESC: This is a template script
+USAGE:"
+  cat << EOF | column -t
+VM=el9-090 IP=192.168.122.90 $sc_name1
+EOF
+exit 1
 }
+
+if [ ! -z "${VM}" -a ! -z "${IP}" ]; then # if required variables are not empty
+  f-marker $sc_name1 $p_all_input_parameters
+else
+  f_use
+fi
+
+v_ip_last=`echo ${IP} | awk -F'.' '{ print $NF }'`
+v_ip_last_padded=`printf "%03d" $v_ip_last`
+virsh list --all | grep "\-$v_ip_last_padded" 2> /dev/null
+v_check_if_similar_vm_exists_based_on_ip=$?
+if [ $v_check_if_similar_vm_exists_based_on_ip -eq 0 ]; then
+  echo -e "\nFAILED. VM ${VM} has similar VM/s running based on IP ${IP}. See above output.\n"
+  exit 1
+fi
+echo -e "PASSED."
