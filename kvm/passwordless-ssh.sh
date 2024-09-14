689bcffe7354a8c3ef9faa711c012faebe196909
totelpo <tots.po@gmail.com>
2024-09-14 13:32:28 +0800

    Update passwordless-ssh.sh

--- kvm/passwordless-ssh.sh
+++ kvm/passwordless-ssh.sh
@@ -13,16 +13,29 @@ OS=el9 IP=192.168.122.90 $sc_name1
 
 if [ ! -z "${OS}" -o ! -z "${IP}" ]; then
   f-marker $sc_name1 IP=${IP}
-  set -e
-  echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${IP}"
-  echo
-  if [ "${OS}" = "el5" ]; then
-          sh -xc "ssh-copy-id -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_rsa_kvm -o HostKeyAlgorithms=+ssh-rsa ${IP}"
-  else
-          sh -xc "ssh-copy-id -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_rsa_kvm ${IP}"
-  fi
-  echo -e "\n# Test passwordless SSH :"
-  sh -xc "ssh ${IP} id"
+
+# Check if SSH connection works without a password
+ssh -o BatchMode=yes -o ConnectTimeout=5 ${IP} 'exit'
+
+# Capture the exit status
+if [ $? -eq 0 ]; then
+  echo "Passwordless SSH is already working!"
+else
+  echo "Passwordless SSH is NOT working. We will proceed to for its setup."
+
+    set -e
+    echo; sh -xc "ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${IP}"
+    echo
+    if [ "${OS}" = "el5" ]; then
+            sh -xc "IP=${IP} SSH_ARGS='-o HostKeyAlgorithms=+ssh-rsa' passwordless-ssh.exp"
+    else
+            sh -xc "IP=${IP} SSH_ARGS='' passwordless-ssh.exp"
+    fi
+    echo -e "\n# Test passwordless SSH :"
+    sh -xc "ssh ${IP} id"
+
+fi
+
 else
   f_use
 fi
