84e8b6688c79628c33ddc0846bfdd33236255ab9
totelpo <tots.po@gmail.com>
2024-09-14 22:52:21 +0800

    Update change-hostname.sh

--- remote/change-hostname.sh
+++ remote/change-hostname.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # totel 20240909 add to github
 sc_name=$0
+
+if [ ! -z "${SUDO_USER}" ]; then
+  echo "
+This script must be properly executed with sudo command. Example :
+sudo bash -c 'OS=el9 NEW_HOSTNAME=el9-091  $(basename ${sc_name}) '
+"
+  exit 1
+fi
 set -e
 source /home/${SUDO_USER}/script/env/env_vm_os_admin.sh
 source ${ENV_DIR}/env_function.sh
@@ -10,13 +18,11 @@ set +e
 f_use(){
 	echo "
 USAGE :
-./$sc_name1 OS  NEW_HOSTNAME
-./$sc_name1 el9 el9-091
+OS=el9 NEW_HOSTNAME=el9-091 ./$sc_name1 OS  NEW_HOSTNAME
 "
 	exit
 }
 
-p_os="$1";   p_os=${p_os:=el7} # default value
 p_new_hostname="$2"
 
 v_log=${sc_tmp}.log
@@ -24,37 +30,37 @@ v_log=${sc_tmp}.log
 if [ $# -eq 2 ]; then
   f-marker $sc_name1 $p_all_input_parameters  # move after all the checks
 	v_old_hostname="`hostname -s`"
-	if [ "$v_old_hostname" = "$p_new_hostname" ]; then
+	if [ "${v_old_hostname}" = "${NEW_HOSTNAME}" ]; then
 		echo -e "\nNo hostname changed. Old and new hostname are the same.\n"
 		exit
 	fi
 
 	f_hostnamectl(){
 		f-marker ${FUNCNAME[0]}
-	        sh -xc "hostnamectl set-hostname $p_new_hostname"  # remove .kvmpo.local to avoid dns search which slow down network search
+	        sh -xc "hostnamectl set-hostname ${NEW_HOSTNAME}"  # remove .kvmpo.local to avoid dns search which slow down network search
 	}
 
 	f_hostname_script(){
 		f-marker ${FUNCNAME[0]}
-		sed -i "s/^HOSTNAME=.*/\HOSTNAME=$p_new_hostname.kvmpo.local/"   /etc/sysconfig/network
-		hostname $p_new_hostname.kvmpo.local
+		sed -i "s/^HOSTNAME=.*/\HOSTNAME=${NEW_HOSTNAME}.kvmpo.local/"   /etc/sysconfig/network
+		hostname ${NEW_HOSTNAME}.kvmpo.local
 	}
 
 	f_ubuntu_hostnamectl(){
 		f-marker ${FUNCNAME[0]}
-	        sh -xc "hostnamectl set-hostname $p_new_hostname.kvmpo.local"
+	        sh -xc "hostnamectl set-hostname ${NEW_HOSTNAME}.kvmpo.local"
 	}
 
 	echo -e "\n# Logging to : ${v_log}\n"
 	set -e
-	if   [ "$p_os" = "el7" -o "$p_os" = "el8" -o "$p_os" = "el9" ]; then
+	if   [ "${OS}" = "el7" -o "${OS}" = "el8" -o "${OS}" = "el9" ]; then
 		f_hostnamectl
-	elif [ "$p_os" = "el5" -o "$p_os" = "el6" ]; then
+	elif [ "${OS}" = "el5" -o "${OS}" = "el6" ]; then
 		f_hostname_script
-	elif [ "$p_os" = "u16" -o "$p_os" = "u20" -o "$p_os" = "u22" ]; then
+	elif [ "${OS}" = "u16" -o "${OS}" = "u20" -o "${OS}" = "u22" ]; then
 		f_ubuntu_hostnamectl
 	else 
-		echo "$p_os : OS not supported"
+		echo "${OS} : OS not supported"
 	fi 2>&1 &> ${v_log}
 else
 	f_use
