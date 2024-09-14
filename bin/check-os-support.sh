34c5ad86450b533afbe8198fb072c061336c741c
totelpo <tots.po@gmail.com>
2024-09-14 22:53:32 +0800

    Add check-os-support.sh

---
+++ bin/check-os-support.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# totel 20240914 
+
+if [ ! -z "${OS}" ]; then
+case ${OS} in
+  el8|el9)
+    echo "${OS} is in the list." > /dev/null
+    ;;
+  u22|u24)
+    echo "${OS} is not yet suppored."
+    exit 1
+    ;;
+  deb11|deb12)
+    echo "${OS} is not yet suppored."
+    exit 1
+    ;;
+  *)
+    echo "${OS} is not suppored."
+    exit 1
+    ;;
+esac
+
+else
+  echo "Empty variable OS=${OS}."
+  exit 1
+fi
