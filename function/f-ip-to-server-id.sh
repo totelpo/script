1be9e0bdb0ffdcab482a0f1b2eb9efc83603b683
totelpo <tots.po@gmail.com>
2024-09-15 21:03:04 +0800

    Add f-ip-to-server-id.sh

---
+++ function/f-ip-to-server-id.sh
@@ -0,0 +1,10 @@
+f-ip-to-server-id(){
+  r_server_id=`echo ${IP} | awk -F'.' '{ print $NF }'`
+  r_server_id_pad=$(printf "%03d\n" $r_server_id)
+  r_ansible_host=s${r_server_id_pad}
+  echo "
+# r_server_id     = $r_server_id
+# r_server_id_pad = $r_server_id_pad
+# r_ansible_host  = ${r_ansible_host}
+"
+}
