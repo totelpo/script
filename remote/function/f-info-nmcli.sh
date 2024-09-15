8837dc963d4f6769bc07149a604ecded28488235
totelpo <tots.po@gmail.com>
2024-09-15 10:52:57 +0800

    Add f-info-nmcli.sh

---
+++ remote/function/f-info-nmcli.sh
@@ -0,0 +1,11 @@
+f-info-nmcli()
+{
+  # This is called by change-ip.sh
+  f-marker ${FUNCNAME[0]}
+  bash -xc "nmcli con show"
+  echo 
+  bash -xc "nmcli -t -f IP4 connection show ${v_conn_name}"
+"
+}
+
+
