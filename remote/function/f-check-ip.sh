d43774f58413e6d704d3bc8d4f4f79d8be3a90f0
totelpo <tots.po@gmail.com>
2024-09-15 10:52:57 +0800

    Add f-check-ip.sh

---
+++ remote/function/f-check-ip.sh
@@ -0,0 +1,20 @@
+
+f-check-ip(){
+  # This is called by change-ip.sh
+  f-marker ${FUNCNAME[0]}
+  echo "${NEW_IP}" | grep '192.168.122\.' > /dev/null
+  if [ $? -gt 0 ]; then
+    echo "Invalid IP ${NEW_IP} . IP must be 192.168.122.X"
+    exit 1
+  fi
+  if [ "${v_old_ip}" = "${NEW_IP}" -o "${NEW_IP}" = "0" ]; then
+    echo -e "\nNo IP changed. Old(${v_old_ip}) and new(${NEW_IP}) IP are the same.\n"
+    exit
+  fi
+  bash -xc "ping -c 1 ${NEW_IP} > /dev/null"
+  if [ $? -eq 0 ]; then
+    echo "\nIP conflict for ${NEW_IP}\n"
+    exit 1
+  fi
+}
+
