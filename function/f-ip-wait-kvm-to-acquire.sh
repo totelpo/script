f66a4dd91218d875d01f04971aefbe76d358fcd5
totelpo <tots.po@gmail.com>
2024-09-16 01:27:08 +0800

    Update f-ip-wait-kvm-to-acquire.sh

--- function/f-ip-wait-kvm-to-acquire.sh
+++ function/f-ip-wait-kvm-to-acquire.sh
@@ -1,22 +1,25 @@
 f-ip-wait-kvm-to-acquire(){
-  f-marker ${FUNCNAME[0]}
-  fi_vm=$1
-  fi_timeout="$2"; fi_timeout=${fi_timeout:=120} # default value
-  echo -e "\nWaiting for $fi_vm to startup and acquire an IP address" 
-  v_timeout0=0
-  v_sleep=5
-  f-get-kvm-ip $fi_vm; v_ip=$r_ip
-  while [ "$v_ip" = "" ]; do
-    f-get-kvm-ip $fi_vm; v_ip=$r_ip
-    printf "."
-    sleep $v_sleep
-    v_timeout0=$((v_timeout0+v_sleep))
-    if [ $v_timeout0 -gt $fi_timeout ]; then
-      echo -e "\nTimeout of $fi_timeout seconds reached.\n"
-      exit 1
-    fi
-  done
-  echo -e "\nVM $fi_vm has acquired IP $r_ip"
-  echo
+  f-marker ${FUNCNAME[0]} VM=${VM} WAIT_MINUTE=${WAIT_MINUTE}
+  if [ ! -z "${VM}" -a ! -z "${WAIT_MINUTE}" ]; then
+    v_timeout=$((WAIT_MINUTE*60))
+    echo -e "\nWaiting for ${VM} to startup and acquire an IP address. WAIT_MINUTE=${WAIT_MINUTE} " 
+    v_timeout0=0
+    v_sleep=5
+    VM=${VM}   f-get-kvm-ip; v_ip=$r_ip
+    while [ "$v_ip" = "" ]; do
+      VM=${VM} f-get-kvm-ip; v_ip=$r_ip
+      printf "."
+      sleep $v_sleep
+      v_timeout0=$((v_timeout0+v_sleep))
+      if [ $v_timeout0 -gt $v_timeout ]; then
+        echo -e "\nTimeout of $v_timeout seconds reached.\n"
+        exit 1
+      fi
+    done
+    echo -e "\nVM ${VM} has acquired IP $r_ip"
+    echo
+  else
+    echo "\nEmpty variable : VM=${VM} | WAIT_MINUTE=${WAIT_MINUTE}\n"
+  fi
 }
-
+  
