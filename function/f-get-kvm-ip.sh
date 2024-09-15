ea63020b16c9af9b6f3f4e5f724d2d566e7cd298
totelpo <tots.po@gmail.com>
2024-09-15 21:02:39 +0800

    Update f-get-kvm-ip.sh

--- function/f-get-kvm-ip.sh
+++ function/f-get-kvm-ip.sh
@@ -1,10 +1,11 @@
 f-get-kvm-ip(){
-	i_vm_name="$1"; i_vm_name=${i_vm_name:=$p_vm_name}
-	i_verbose=$2
-	v_mac=`virsh domiflist $i_vm_name | awk '{ print $5 }' | grep . | sed '1d'`
-	r_ip=`arp -an | grep "$v_mac" | awk '{ print $2}' | sed 's/(//; s/)//'`
-	if [ "$i_verbose" = v ]; then
-		echo -e "# r_ip = $r_ip"
-	fi
+  v_mac=`virsh domiflist ${VM} | awk '{ print $5 }' | grep . | sed '1d'`
+  r_ip=`arp -an | grep "${v_mac}" | awk '{ print $2}' | sed 's/(//; s/)//'`
+  if [ ! -z "${r_ip}" ]; then
+    echo -e "# r_ip = $r_ip "
+    IP=$r_ip f-ip-to-server-id
+  else
+    echo "KVM guest(${VM}) has no IP: r_ip=${r_ip} ." 
+  fi
 }
 
