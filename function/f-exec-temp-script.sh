684002527f149cfbcf72b537a352aecaf951ddeb
totelpo <tots.po@gmail.com>
2024-09-16 17:37:54 +0800

    Update f-exec-temp-script.sh

--- function/f-exec-temp-script.sh
+++ function/f-exec-temp-script.sh
@@ -4,7 +4,10 @@ fn_name=${FUNCNAME[0]}
 fi_tmp_sh=$1
 if [ $# -eq 1 ]; then
   f-marker $fn_name $@
+  v_marker_width=$((COLUMNS/4))
+  eval "printf '#%.0s' {1..$v_marker_width}"
 	echo ; bash -xc "cat $fi_tmp_sh"
+  eval "printf '#%.0s' {1..$v_marker_width}"
 	if [ "$EXEC" = "y" ]; then
                 echo; source $fi_tmp_sh
                 bash -xc "sleep 2"
