a29907cf86a60ac1cf51c1a8ee36b821576917a1
totelpo <tots.po@gmail.com>
2024-09-16 11:51:24 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# DESC: This is a template script
 # totel 20240913 Some info here
+# totel 20240816 v2.0 Change input from Positional Parameters to Environment Variable Assignment (Inline Export)
 
 sc_name=$0
 source ${ENV_DIR}/env_function.sh
 source ${ENV_DIR}/env_script.sh
 
 f_use(){
-          echo "
- DESC: This is a template script
-USAGE:"
+  echo "
+# USAGE:"
   cat << EOF | column -t
 EXEC=y VM=el9-090 IP=192.168.122.90 $sc_name1
 EOF
@@ -26,8 +27,7 @@ else
   # Ensure required variables are defined
   echo "
 FAILED: Empty variables found.
-VM=${VM} | IP=${IP}
-"
+VM=${VM} | IP=${IP} "
   f_use
 fi
 
