b6123a55c28aabde6da991e8ebec0f4c87a44429
totelpo <tots.po@gmail.com>
2024-09-16 17:37:54 +0800

    Update vm-arp-clear-unreachable-ip.sh

--- kvm/vm-arp-clear-unreachable-ip.sh
+++ kvm/vm-arp-clear-unreachable-ip.sh
@@ -9,10 +9,9 @@ v_log=$sc_tmp.log
 echo -e "\n# Clearing ARP cache for non-reachable IP in range '192.168.122.*' :" 
 arp -n | grep virbr0 | grep 192.168.122 > $v_log && (
 	cat $v_log
-  echo 
+	echo 
 	for i in `awk '{ print $1 }' $v_log`; do
 		ping -c 1 $i > /dev/null || (
-			echo -e "## Clearing for $i"
 			sh -xc "sudo arp -d $i"
 			)
 	done
