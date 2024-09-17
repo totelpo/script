a7ec505f51024765eb8dc757d768944c24ecbb8e
totelpo <tots.po@gmail.com>
2024-09-17 16:07:10 +0800

    Update f-marker.sh

--- function/f-marker.sh
+++ function/f-marker.sh
@@ -7,12 +7,7 @@ f-date-host(){
 }
 
 f-marker (){
-p_marker_id="$(echo $@ | sed "s|${HOME}/|~/|g")"
-  export TERM=xterm
-# MARKER_WIDTH="${MARKER_WIDTH:=100}"   # OR `tput cols`  # MARKER_WIDTH=105 for main script
-MARKER_WIDTH="${MARKER_WIDTH:=$((`tput cols`-5))}"   # OR `tput cols`  # MARKER_WIDTH=105 for main script
-  v_marker=`eval "printf '#%.0s' {1..$MARKER_WIDTH}"`
- #echo -e "${v_marker}"
-  echo -e "\n# $(f-date-host) $p_marker_id $v_marker" | cut -c 1-$MARKER_WIDTH
- #echo -e "${v_marker}"
+  p_marker_id="$(echo $@ | sed "s|${HOME}/|~/|g")"
+  echo -e "\n# $(f-date-host) ${p_marker_id} ${MARKER}" | cut -c 1-$MARKER_WIDTH
+ #echo -e "${MARKER}"
 }
