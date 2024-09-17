58e85d82d5b323afa475c017e47e9e1c28c9c354
totelpo <tots.po@gmail.com>
2024-09-17 22:01:27 +0800

    Update f-info-ip.sh

--- remote/function/f-info-ip.sh
+++ remote/function/f-info-ip.sh
@@ -2,7 +2,7 @@ f-info-ip()
 {
   # This is called by change-ip.sh
   f-marker ${FUNCNAME[0]}
-  bash -xc "ip -4 addr show  dev ${v_dev}"
+  bash -xc "ip -4 addr show  dev ${NETWORK_DEVICE}"
   echo
   bash -xc "lshw -class network -short"
 }
