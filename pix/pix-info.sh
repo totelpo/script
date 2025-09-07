fc9b4127a0207730d324bfd79e1337f3254985fa
totelpo <tots.po@gmail.com>
2025-09-07 15:30:41 +0800

    Update pix-info.sh

--- pix/pix-info.sh
+++ pix/pix-info.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sc_name=$(basename $0)
-sc_name1="${sc_name%.*}" # without extension name
-
-sc_tmp=${TMPDIR}/${sc_name}.tmp
 
 <<COMMENT
 20240827 totel Add to github
@@ -12,14 +9,15 @@ if [ $# -lt 1 ]; then
   echo "
  DESC: Get photo info
 USAGE: 
-${sc_name} INPUT/s 
-${sc_name} 2645-payapag/2645-010\*.jpg 
+HIDE=-H5 ${sc_name} INPUT/s 
+HIDE=-H5 ${sc_name} 2645-payapag/2645-010\*.jpg 
 "
 else 
   (
-  echo "Filesize | Filename | WxH | DPI"
-    for i in $@; do
-      identify -format "`du -sh ${i}` | %w x %h | %x x %y\n" ${i} 2> /dev/null; 
-    done | sed "s|$HOME|~|" | column -t | tr -s ' ' | sed 's/ / | /'  
-  ) | column -t -s'|' -o'|'
+  echo "Filesize|Filename|Width|Height|DPI"
+    for filename in $@; do
+      identify -format "`du -sh ${filename}`|%w|%h|%xx%y\n" ${filename}; 
+    done | sed "s|$HOME|~|" | column -t | tr -s ' ' | sed 's/ /|/'  
+  ) | awk -F'|' 'NR==1 { for(i=1;i<=NF;i++) printf i (i==NF ? "\n" : "|") } {print}' `# Add column numbers` \
+    | column -t -s'|' -o'|' ${HIDE}
 fi
